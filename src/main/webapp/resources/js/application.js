65b78c7a7f6abb61f6eb8208859140cbc36e651b
Yuan Ji <jiwhiz@gmail.com>
2013-04-21 11:54:27 -0600

    fix css
    change side bar link color to default;
    change tag label class to label-info;
    change button class to btn-primary;
    change tag cloud color to blue;
    change social icon color to blue;

--- src/main/webapp/resources/js/application.js
+++ src/main/webapp/resources/js/application.js
@@ -1,7 +1,7 @@
 //Tag Cloud setup
 $.fn.tagcloud.defaults = {
 	size: {start:10, end: 20, unit: 'pt'},
-	color: {start: '#00CC66', end: '#006600'}
+	color: {start: '#0099FF', end: '#0033FF'}
 };
 
 $(function () {
